100ba884038616f2960940223f1fa5051ecae093
sukebon <39957486+sukebon@users.noreply.github.com>
2023-05-27 09:29:44 +0900

    scan 効果音

---
+++ src/main.js
@@ -0,0 +1,49 @@
+window.addEventListener('load', function () {
+  function audio() {
+    document.getElementById('btn_audio').currentTime = 0;
+    document.getElementById('btn_audio').play();
+    console.log("audio");
+  }
+  let selectedDeviceId;
+  const codeReader = new ZXing.BrowserMultiFormatReader();
+  codeReader.listVideoInputDevices()
+    .then((videoInputDevices) => {
+
+      selectedDeviceId = videoInputDevices[0].deviceId;
+      if (videoInputDevices.length >= 1) {
+        console.log("deviceId", videoInputDevices[0].deviceId);
+        console.log("deviceLabel", videoInputDevices[0].label);
+      }
+      let array = [];
+      document.getElementById('startButton').addEventListener('click', () => {
+        codeReader.decodeFromVideoDevice(undefined, 'video', (result, err) => {
+          if (result) {
+            console.log("result", result);
+            const isInclude = array.includes(result.text);
+            if (isInclude) return;
+            array.push(result.text);
+            audio();
+            let li = document.createElement('li');
+            li.textContent = result.text;
+            document.getElementById('result').appendChild(li);
+          }
+          if (err && !(err instanceof ZXing.NotFoundException)) {
+            console.error(err);
+          }
+        });
+      });
+      document.getElementById('resetButton').addEventListener('click', () => {
+        codeReader.reset();
+        document.getElementById('result').remove();
+        let ul = document.createElement("ul");
+        ul.id = "result";
+        ul.classList.add(["p-2"]);
+        document.getElementById('list-area').appendChild(ul);
+        array = [];
+      });
+
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
